ceed8261766449aa7ad261c65b798fd1683555e4
Gersey Stelmach <gerseystelmach@gmail.com>
2021-09-16 14:18:55 +0200

    fix navbar sandwich icon

--- template.php
+++ template.php
@@ -9,7 +9,7 @@
 	<title>Orléans</title>
 </head>
 <body>
-	<nav class="navbar navbar-expand-lg">
+	<nav class="navbar  navbar-expand-lg">
 		<div class="container-fluid ">
 		<a class="navbar-brand" href="index.php"><img src="/assets/images/Logo_Orléans_Métropole_blanc.png" alt="logo Orléans blanc"> </a>
 		  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -25,9 +25,6 @@
 			  </li>
 			  <li class="nav-item">
 				<a class="nav-link" href="tourisme.php">Tourisme</a>
-			  </li>
-			  <li class="nav-item">
-				<a class="nav-link" href="evenements.php">Evénements</a>
 			  </li>
 			  <li class="nav-item">
 				<a class="nav-link" href="galerie.php">Galerie</a>
